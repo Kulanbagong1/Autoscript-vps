618dbcfeb6f6df48b699ea094c60a98237ee02ef
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-22 04:45:04 +0700

    Update ub20.sh
    Signed-off-by: Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -38,6 +38,12 @@ RAMMS=$(free -m | awk 'NR==2 {print $2}')
 KEY="6149751863:AAH_trZtL0Y6NMskF6nxeVGZgFpowXi85uc"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
 GITHUB_CMD="https://github.com/Kulanbagong1/Autoscript-vps/raw/"
+#NAMECOM=$(curl -sS https://sc-xray.yha.my.id/ip | grep $MYIP | awk '{print $2}')
+OS=$(cat /etc/os-release | grep -w PRETTY_NAME | head -n1 | sed 's/=//g' | sed 's/"//g' | sed 's/PRETTY_NAME//g')
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+Date_list=$(date +"%Y-%m-%d" -d "$dateFromServer")
+#echo $NAMECOM >/usr/local/etc/.$NAMECOM.ini
+#CekOne=$(cat /usr/local/etc/.$NAMECOM.ini)
 Repo1="https://raw.githubusercontent.com/Kulanbagong1/izinn/main/"
 
 localip=$(hostname -I | cut -d\  -f1)
