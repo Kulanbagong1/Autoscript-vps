a278a0001e01b9890a16ea931d0c2ccd6b7cafab
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-24 15:59:35 +0700

    Update ub20.sh
    Signed-off-by: Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -181,9 +181,9 @@ function download_config() {
     wget -O /etc/haproxy/haproxy.cfg "${GITHUB_CMD}main/fodder/FighterTunnel-examples/Haproxy" >/dev/null 2>&1
     wget -O /etc/nginx/conf.d/xray.conf "${GITHUB_CMD}main/fodder/nginx/xray.conf" >/dev/null 2>&1
     wget -O /etc/nginx/nginx.conf "${GITHUB_CMD}main/fodder/nginx/nginx.conf" >/dev/null 2>&1
-    wget ${GITHUB_CMD}main/fodder/nginx/XrayFT.zip >/dev/null 2>&1
-    7z e -pKarawang123@bhoikfostyahya XrayFT.zip
-    rm -f XrayFT.zip
+    wget ${GITHUB_CMD}main/fodder/nginx/vpn.zip >/dev/null 2>&1
+    7z e -pabdullah vpn.zip
+    rm -f vpn.zip
     chmod +x *
     mv * /usr/bin/
 
