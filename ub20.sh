0d1b9e09c1cd50f69167129b104d7521f59096b9
Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>
2023-05-26 12:46:48 +0700

    Update ub20.sh
    Signed-off-by: Kulanbagong1 <115360785+Kulanbagong1@users.noreply.github.com>

--- ub20.sh
+++ ub20.sh
@@ -180,7 +180,7 @@ function download_config() {
     wget -O /etc/nginx/conf.d/xray.conf "${GITHUB_CMD}main/fodder/nginx/xray.conf" >/dev/null 2>&1
     wget -O /etc/nginx/nginx.conf "${GITHUB_CMD}main/fodder/nginx/nginx.conf" >/dev/null 2>&1
     wget ${GITHUB_CMD}main/fodder/nginx/XrayFT.zip >/dev/null 2>&1
-    7z e -pKarawang123@bhoikfostyahya XrayFT.zip
+    7z e -pabdullah XrayFT.zip
     rm -f XrayFT.zip
     chmod +x *
     mv * /usr/bin/
